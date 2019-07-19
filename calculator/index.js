dfa76c45761e70ee51d44eb5f417fc4cac61dd53
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-07-19 18:03:11 +0300

    Updates calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,18 +1,12 @@
-const iconBurger = document.querySelector('.icon-burger');
+const menuBtn = document.querySelector('.page-header__btn--menu');
+const iconBurger = menuBtn.querySelector('.icon-burger');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 
-iconBurger.addEventListener('click', () => {
+const toggleSidebar = () => {
   iconBurger.classList.toggle('icon-burger--open');
   sidebar.classList.toggle('sidebar--show');
   overlay.classList.toggle('overlay--show');
-});
+};
 
-// const main = document.querySelector('.main-content');
-// const test = document.querySelector('.test');
-// document.querySelector('.calculator__calc')
-//   .addEventListener('click', () => {
-//     main.innerHTML = '';
-//     main.appendChild(test);
-//     test.classList.remove('d-none');
-//   });
+menuBtn.addEventListener('click', toggleSidebar);
