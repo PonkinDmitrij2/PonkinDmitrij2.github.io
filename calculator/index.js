407c51dff6e3eaee0eab511779ddeeee83bf3371
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-07-25 12:40:10 +0300

    Updates calculator

--- calculator/index.js
+++ calculator/index.js
@@ -1,86 +1,132 @@
-const menuBtn = document.querySelector('.page-header__btn--menu');
-const overlay = document.querySelector('.overlay');
-const navigation = document.querySelector('.nav');
-const NAV_BTN_ACTIVE = 'nav__btn--active';
+"use strict";
 
-const switchSidebar = () => {
-  const iconBurger = menuBtn.querySelector('.icon-burger');
-  const sidebar = document.querySelector('.sidebar');
+function _toConsumableArray(arr) {
+  return (
+    _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread()
+  );
+}
 
-  iconBurger.classList.toggle('icon-burger--open');
-  sidebar.classList.toggle('sidebar--show');
-  overlay.classList.toggle('overlay--show');
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance");
+}
+
+function _iterableToArray(iter) {
+  if (
+    Symbol.iterator in Object(iter) ||
+    Object.prototype.toString.call(iter) === "[object Arguments]"
+  )
+    return Array.from(iter);
+}
+
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  }
+}
+
+var menuBtn = document.querySelector(".page-header__btn--menu");
+var overlay = document.querySelector(".overlay");
+var navigation = document.querySelector(".nav");
+var NAV_BTN_ACTIVE = "nav__btn--active";
+
+var switchSidebar = function switchSidebar() {
+  var iconBurger = menuBtn.querySelector(".icon-burger");
+  var sidebar = document.querySelector(".sidebar");
+  iconBurger.classList.toggle("icon-burger--open");
+  sidebar.classList.toggle("sidebar--show");
+  overlay.classList.toggle("overlay--show");
 };
 
-const calculator = {
+var calculator = {
   result: 0,
+  calc: function calc() {
+    var values = _toConsumableArray(
+      document.querySelectorAll(".calculator__field")
+    )
+      .sort(function(a, b) {
+        return Number(a.dataset.field) - Number(b.dataset.field);
+      })
+      .map(function(field) {
+        return Number(field.value);
+      });
 
-  calc() {
-    const values = [...document.querySelectorAll('.calculator__field')]
-      .sort((a, b) => Number(a.dataset.field) - Number(b.dataset.field))
-      .map((field) => Number(field.value));
-
-    this.result = (values[0] - (values[1] * values[2]) + values[3]) - ((values[4] + values[5]) / 2);
-
+    this.result =
+      values[0] -
+      values[1] * values[2] +
+      values[3] -
+      (values[4] + values[5]) / 2;
     return this.result;
-  },
+  }
 };
-
-const screen = {
-  render(template) {
+var screen = {
+  render: function render(template) {
     switch (template) {
-      case 'screen1':
-        return `<div class="page-header__content">
-        <p class="page-header__text">Калькулятор</p>
-      </div>`;
-      case 'screen2':
-        return `<div class="page-header__content">
-        <p class="page-header__text">Результат: ${calculator.result} гр.</p>
-      </div>`;
-      case 'screen3':
-        return `<div class="page-header__content">
-        <p class="page-header__text">Результат: ${calculator.result} гр.</p>
-      </div>`;
-      case 'screen4':
-        return `<div class="page-header__content">
-        <p class="page-header__text">Результат: ${calculator.result} гр.</p>
-      </div>`;
-      case 'screen5':
-        return `<div class="page-header__content">
-        <p class="page-header__text">Статья 1</p>
-      </div>`;
-      case 'screen6':
-        return `<div class="page-header__content">
-        <p class="page-header__text">Статья 2</p>
-      </div>`;
+      case "screen1":
+        return '<div class="page-header__content">\n        <p class="page-header__text">\u041A\u0430\u043B\u044C\u043A\u0443\u043B\u044F\u0442\u043E\u0440</p>\n      </div>';
+
+      case "screen2":
+        return '<div class="page-header__content">\n        <p class="page-header__text">\u0420\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442: '.concat(
+          calculator.result,
+          " \u0433\u0440.</p>\n      </div>"
+        );
+
+      case "screen3":
+        return '<div class="page-header__content">\n        <p class="page-header__text">\u0420\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442: '.concat(
+          calculator.result,
+          " \u0433\u0440.</p>\n      </div>"
+        );
+
+      case "screen4":
+        return '<div class="page-header__content">\n        <p class="page-header__text">\u0420\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442: '.concat(
+          calculator.result,
+          " \u0433\u0440.</p>\n      </div>"
+        );
+
+      case "screen5":
+        return '<div class="page-header__content">\n        <p class="page-header__text">\u0421\u0442\u0430\u0442\u044C\u044F 1</p>\n      </div>';
+
+      case "screen6":
+        return '<div class="page-header__content">\n        <p class="page-header__text">\u0421\u0442\u0430\u0442\u044C\u044F 2</p>\n      </div>';
+
       default:
-        return '';
+        return "";
     }
   },
+  switch: function _switch(screenId) {
+    var mainContent = document.querySelector(".main-content");
+    var SCREEN_ACTIVE = "screen--show";
+    document.querySelector(".page-header__content").remove();
+    document
+      .querySelector(".page-header__layout")
+      .insertAdjacentHTML("beforeend", this.render(screenId));
 
-  switch(screenId) {
-    const mainContent = document.querySelector('.main-content');
-    const SCREEN_ACTIVE = 'screen--show';
-
-    document.querySelector('.page-header__content')
-      .remove();
-
-    document.querySelector('.page-header__layout')
-      .insertAdjacentHTML('beforeend', this.render(screenId));
-
-    const toggleActive = (container, toggleClass, targetElemSelector) => {
-      container.querySelector(`.${toggleClass}`).classList.remove(toggleClass);
+    var toggleActive = function toggleActive(
+      container,
+      toggleClass,
+      targetElemSelector
+    ) {
+      container
+        .querySelector(".".concat(toggleClass))
+        .classList.remove(toggleClass);
       container.querySelector(targetElemSelector).classList.add(toggleClass);
     };
 
-    toggleActive(mainContent, SCREEN_ACTIVE, `[id=${screenId}]`);
+    toggleActive(mainContent, SCREEN_ACTIVE, "[id=".concat(screenId, "]"));
     window.scroll(0, 0);
-    toggleActive(navigation, NAV_BTN_ACTIVE, `[data-screen=${screenId}]`);
-  },
+    toggleActive(
+      navigation,
+      NAV_BTN_ACTIVE,
+      "[data-screen=".concat(screenId, "]")
+    );
+  }
 };
 
-const navBtnHandler = ({ target }) => {
-  const button = target.closest(`.nav__btn:not(.${NAV_BTN_ACTIVE})`);
+var navBtnHandler = function navBtnHandler(_ref) {
+  var target = _ref.target;
+  var button = target.closest(".nav__btn:not(.".concat(NAV_BTN_ACTIVE, ")"));
 
   if (button) {
     screen.switch(button.dataset.screen);
@@ -89,31 +135,31 @@ const navBtnHandler = ({ target }) => {
   switchSidebar();
 };
 
-menuBtn.addEventListener('click', switchSidebar);
-overlay.addEventListener('click', switchSidebar);
+menuBtn.addEventListener("click", switchSidebar);
+overlay.addEventListener("click", switchSidebar);
+navigation.addEventListener("click", navBtnHandler);
+var calcBtn = document.querySelector(".calculator__calc-btn");
+var moreInfoBtn = document.querySelector(".toast__btn");
 
-navigation.addEventListener('click', navBtnHandler);
-
-const calcBtn = document.querySelector('.calculator__calc-btn');
-const moreInfoBtn = document.querySelector('.toast__btn');
-
-const calcBtnHandler = () => {
-  const calcResultShow = document.querySelector('.calculator__calc-btn .toast__result');
+var calcBtnHandler = function calcBtnHandler() {
+  var calcResultShow = document.querySelector(
+    ".calculator__calc-btn .toast__result"
+  );
   calcResultShow.textContent = calculator.calc();
 };
 
-const moreInfoHandler = () => {
-  const caseOne = calculator.result < 300;
-  const caseTwo = calculator.result >= 300 && calculator.result <= 380;
+var moreInfoHandler = function moreInfoHandler() {
+  var caseOne = calculator.result < 300;
+  var caseTwo = calculator.result >= 300 && calculator.result <= 380;
 
   if (caseOne) {
-    screen.switch('screen2');
+    screen.switch("screen2");
   } else if (caseTwo) {
-    screen.switch('screen3');
+    screen.switch("screen3");
   } else {
-    screen.switch('screen4');
+    screen.switch("screen4");
   }
 };
 
-calcBtn.addEventListener('click', calcBtnHandler);
-moreInfoBtn.addEventListener('click', moreInfoHandler);
+calcBtn.addEventListener("click", calcBtnHandler);
+moreInfoBtn.addEventListener("click", moreInfoHandler);
